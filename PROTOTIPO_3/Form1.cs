54a10bc7b8674a2cfef85d4dd3353a8aa1cab3f4
Ronaldo P Jovine <1108962@est.intec.edu.do>
2023-01-15 12:50:41 -0400

    IA 1.0v Sujeta a cambios

--- PROTOTIPO_3/Form1.cs
+++ PROTOTIPO_3/Form1.cs
@@ -57,19 +57,19 @@ namespace PROTOTIPO_3
 
             /*FUNCIONES BASICAS DE LA IA 42 (Saludos,Preguntas y Respuestas)------------------*/
 
-            if (e.Result.Text == "hola 42" || e.Result.Text == "42")
-            {   
+            if (e.Result.Text == "hola 42" || e.Result.Text == "42" || e.Result.Text == "o la 42" || e.Result.Text == "horas 42" || e.Result.Text == "ola" || e.Result.Text == "o la" || e.Result.Text == "hola")
+                {   
                 hablar.SpeakAsync($"Hola {nombre} ¿Comó puedo ayudarte?");
             }
 
-            if (e.Result.Text == "quién eres" || e.Result.Text == "hola")
+            if (e.Result.Text == "quién eres" || e.Result.Text == "a quien eres?" || e.Result.Text == "a quién eres")
             {
                 hablar.SpeakAsync($"Hola {nombre}, soy tu asistente virtual opiripiropi 42, ¿En que puedo ayudarte?");
             }
 
             /*TEMPERATURA ACTUAL-------------------------------*/
 
-            if (e.Result.Text == "temperatura" || e.Result.Text == "temperatura actual")
+            if (e.Result.Text == "temperatura" || e.Result.Text == "temperatura actual" || e.Result.Text == "cuál es la temperatura" || e.Result.Text == "Cuales temperatura" || e.Result.Text == "Hace calor" || e.Result.Text == "Grados" || e.Result.Text == "grados" || e.Result.Text == "Hace calor fuera" || e.Result.Text == "calor")
             {
                 hablar.SpeakAsync($"La temperatura actual es de {temperature} grados centigrados");
             }
@@ -91,7 +91,7 @@ namespace PROTOTIPO_3
 
             }
 
-            if (e.Result.Text == "Buenos dias" || e.Result.Text == "Buenos dias ")
+            if (e.Result.Text == "Buenos dias" || e.Result.Text == "Buenos días" || e.Result.Text == "Buenos vías")
             {
                 hablar.SpeakAsync($"Buenos dias {nombre}, Luces encedidas");
 
@@ -106,7 +106,7 @@ namespace PROTOTIPO_3
 
             /*RELAY 1 LUZ NORMAL y RELAY 2 LUCES DE FIESTA*/
 
-            if (e.Result.Text == "42 enciende las luces" || e.Result.Text == "enciende luces")
+            if (e.Result.Text == "42 enciende las luces" || e.Result.Text == "enciende luces" || e.Result.Text == "Prende las luces")
             {
                 hablar.SpeakAsync($"Listo {nombre}, luces encendidas");
             }
@@ -115,13 +115,13 @@ namespace PROTOTIPO_3
 
             /*RELAY 1 LUZ NORMAL*/
 
-            if (e.Result.Text == "42 apagar la luz" || e.Result.Text == "apagar la luz")
+            if (e.Result.Text == "42 apagar la luz" || e.Result.Text == "apagar la luz" || e.Result.Text == "apagar luces" || e.Result.Text == "Apaga las luces")
             {
                 serialPort1.Write("8");
                 hablar.SpeakAsync($"Luz apagada {nombre}");
             }
 
-            if (e.Result.Text == "Buenas noches" || e.Result.Text == "Buenas noches")
+            if (e.Result.Text == "Buenas noches" || e.Result.Text == "Buenas noches" || e.Result.Text == "buenas noches")
             {
                 hablar.SpeakAsync($"Luces apagadas, descansa {nombre}");
             }
@@ -129,33 +129,33 @@ namespace PROTOTIPO_3
 
             /*RELAY 2 LUCES DE FIESTA*/
 
-            if (e.Result.Text == "42 apaga la luz de fiesta" || e.Result.Text == "apaga la luz de fiesta")
+            if (e.Result.Text == "42 apaga la luz de fiesta" || e.Result.Text == "apaga la luz de fiesta" || e.Result.Text == "apaga la luz de fiestas")
             {
                 hablar.SpeakAsync($"Luz de fiesta apagada {nombre}");
             }
 
             /*RELAY 1 LUZ NORMAL y RELAY 2 LUCES DE FIESTA*/
 
-            if (e.Result.Text == "42 apaga las luces" || e.Result.Text == "apaga luces" || e.Result.Text == "a pagar luces")
+            if (e.Result.Text == "42 apaga las luces" || e.Result.Text == "apaga luces" || e.Result.Text == "a pagar luces" || e.Result.Text == "apagar las luces")
                     {
                 hablar.SpeakAsync($"Luces apagas {nombre}");
             }
 
             /*ENCENDER LEDS ----------------------------------*/
 
-            if (e.Result.Text == "42 enciende rojo" || e.Result.Text == "enciende rojo")
+            if (e.Result.Text == "42 enciende rojo" || e.Result.Text == "enciende rojo" || e.Result.Text == "prende rojo" || e.Result.Text == "prende luz roja" || e.Result.Text == "enciende luz roja")
             {
                 serialPort1.Write("1");
                 hablar.SpeakAsync($"Led rojo encendido {nombre}");
             }
 
-            if (e.Result.Text == "42 enciende verde" || e.Result.Text == "enciende verde")
+            if (e.Result.Text == "42 enciende verde" || e.Result.Text == "enciende verde" || e.Result.Text == "prende verde" || e.Result.Text == "prende luz verde" || e.Result.Text == "enciende luz verde")
             {
                 serialPort1.Write("2");
                 hablar.SpeakAsync($"Led verde encendido {nombre}");
             }
 
-            if (e.Result.Text == "42 enciende azul" || e.Result.Text == "enciende azul")
+            if (e.Result.Text == "42 enciende azul" || e.Result.Text == "enciende azul" || e.Result.Text == "prende azul" || e.Result.Text == "prende luz azul" || e.Result.Text == "enciende luz azul")
             {
                 serialPort1.Write("3");
                 hablar.SpeakAsync($"Led azul encendido {nombre}");
@@ -181,7 +181,9 @@ namespace PROTOTIPO_3
                 hablar.SpeakAsync($"Led azul apagado {nombre}");
             }
 
-            if (e.Result.Text == "fiesta" || e.Result.Text == "baile" || e.Result.Text == "bailar" || e.Result.Text == "Prende el musicon" || e.Result.Text == "Enciende la fiestas")
+            /*PONE MUSICA ----------------------------------*/
+
+            if (e.Result.Text == "fiesta" || e.Result.Text == "baile" || e.Result.Text == "bailar" || e.Result.Text == "Prende el musicon" || e.Result.Text == "Enciende la fiestas" || e.Result.Text == "a fiesta")
             {
                 hablar.SpeakAsync($"Hora de la fiesta {nombre}");
                 string url = "https://www.youtube.com/watch?v=KJ5zaSPjC6w&ab_channel=DjDashPeru";
@@ -194,10 +196,10 @@ namespace PROTOTIPO_3
                 string url = "https://www.youtube.com/watch?v=3-4banibETY&list=PL6W2JmY9MGbV1ff53Vd0MHMpMM7aUHRnL&ab_channel=Meltt";
                 System.Diagnostics.Process.Start(url);
             }
-            if (e.Result.Text == "Musica clasica" || e.Result.Text == "Musica clasica" || e.Result.Text == "Clasicos")
+            if (e.Result.Text == "Musica clasica" || e.Result.Text == "Clasicos" || e.Result.Text == "Música clásica")
             {
                 hablar.SpeakAsync($"Haz escuchado estos temas {nombre}?");
-                string url = "https://www.youtube.com/watch?v=KJ5zaSPjC6w&ab_channel=DjDashPeru";
+                string url = "https://www.youtube.com/watch?v=fBJVaJyTMx8&list=PLTW24CNetpwQ-o_Fx5kKKOhGkoYhM0RAJ";
                 System.Diagnostics.Process.Start(url);
             }
 
